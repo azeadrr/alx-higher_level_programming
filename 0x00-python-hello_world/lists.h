ceb18d80e2a7b5e29be3d9f7650f3c4a5e35cfbd
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-06-05 07:50:37 -0700

    0x00-python-hello_world

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -4,22 +4,22 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - Unidirectional linked list
- * @num: integer
- * @next: References the next node
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Description: Singly linked list node schema for Holberton project
+ * Description: singly linked list node structure
+ * 
  */
 typedef struct listint_s
 {
-	int num;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int num);
+listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
-
