82e528b3d2c232de3b099a8a406ae387c3bd97ab
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-06-14 15:52:12 -0700

    updated

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,40 +1,15 @@
 #!/usr/bin/python3
-def to_subtract(lst_num):
-    sub = 0
-    max_lst = max(lst_num)
-
-    for n in lst_num:
-        if max_lst > n:
-            sub += n
-
-    return (max_lst - sub)
-
-
-def roman_to_int(roman_string):
-    if not roman_string:
+def roman_to_int(roman_string: str):
+    if roman_string is None or type(roman_string) != str:
         return 0
+    rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = [rom[x] for x in roman_string] + [0]
+    r = 0
 
-    if not isinstance(roman_string, str):
-        return 0
+    for i in range(len(num) - 1):
+        if num[i] >= num[i+1]:
+            r += num[i]
+        else:
+            r -= num[i]
 
-    rom_nu = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    lst_keys = list(rom_n.keys())
-
-    num = 0
-    last_rom = 0
-    lst_num = [0]
-
-    for ch in roman_string:
-        for re_num in lst_keys:
-            if re_num == ch:
-                if rom_nu.get(ch) <= last_rom:
-                    num += to_subtract(lst_num)
-                    lst_num = [rom_nu.get(ch)]
-                else:
-                    lst_num.append(rom_nu.get(ch))
-
-                last_rom = rom_nu.get(ch)
-
-    num += to_subtract(lst_num)
-
-    return (num)
+    return r
