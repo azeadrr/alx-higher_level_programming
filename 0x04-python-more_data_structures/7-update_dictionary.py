9d821613e8939f700f2b31d3634708c1ecfe6e39
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-06-14 15:41:34 -0700

    updated

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
-def update_dictionary(a_dictionary, key, value):
-    """Replace or add value in dictionary."""
-    a_dictionary[key] = value
-    return (a_dictionary)
+#!/usr/bin/python3
+def update_dictionary(my_dict, key, value):
+    my_dict[key] = value
+    return my_dict
