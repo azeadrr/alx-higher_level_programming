e3ccdf20c7abd135b72e1280045fee3655162771
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-06-26 19:10:55 -0700

    updated

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 import sys
+
 def safe_print_integer_err(value):
     """
     "{:d}".format() to print as integer
     value: The integer
-
     Returns: True if value has been correctly printed,
     Otherwise, returns False and prints in stderr the error precede by Exception
     """
