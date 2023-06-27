b5b5681e857da31c562f899b66925048e1fc4eba
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-06-27 16:52:36 -0700

    updated

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Square that defines a square"""
+
+
+class Square:
+    """square class"""
+
+    def __init__(self, size):
+        """
+        size: The integer value of size of the new square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """set size of the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return area"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print square with # """
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
