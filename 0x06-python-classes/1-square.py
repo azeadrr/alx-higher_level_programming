073749410451f520529bb4f014491f6d828ffbe0
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-06-27 15:19:21 -0700

    updated

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Square class"""
+"""Square that defines a square"""
 
 
 class Square:
+    """square class"""
     def __init__(self, size):
+        """ size: The integer value size of new square."""
         self.__size = size
