bda58e4894996b13afb8d97f66fe38bdfd99e44d
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-06-26 18:47:54 -0700

    0x05-python-exceptions advanced tasks

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+def safe_print_integer_err(value):
+    """
+    "{:d}".format() to print as integer
+    value: The integer
+
+    Returns: True if value has been correctly printed,
+    Otherwise, returns False and prints in stderr the error precede by Exception
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+def safe_function(fct, *args):
+    """
+    executes a function safely.
+    fct: a pointer to a function
+    args: the arguments for fct.
+    Returns: if something happens during the function and prints in stderr the error precede by Exception
+    """
+    try:
+        result = fct(*args)
+        return (result)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
