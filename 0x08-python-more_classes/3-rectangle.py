45e1d78a1f690d616dac1b4cb8b0d7b52f288b50
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-07-03 13:14:23 -0700

    updated

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -39,11 +39,23 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Return area of rectangle"""
+        """return area of rectangle"""
         return (self.__width * self.__height)
 
     def perimeter(self):
-        """Return perimeter of rectangle"""
+        """return perimeter of rectangle"""
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """represent rectangle with # character"""
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+
+        rect = []
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
