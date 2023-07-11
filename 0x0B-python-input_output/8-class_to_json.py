50acee711e66d15ddc966945427207d6ef53c3ba
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-07-11 14:41:56 -0700

    updated

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""function that returns the dictionary description with simple data structure"""
+"""Class to JSON"""
 
 
 def class_to_json(obj):
-    """return: a dictionary represntation of simple data structure"""
+    """return: dictionary description with simple data structure """
     return obj.__dict__
