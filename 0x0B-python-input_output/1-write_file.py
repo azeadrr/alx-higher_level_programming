08e244baeafa30190eed9f884505bccbeb4ebe8c
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-07-11 09:35:21 -0700

    mentadory task

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function that writes a string to a text file (UTF8)"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8)
+	return: the number of characters written
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
