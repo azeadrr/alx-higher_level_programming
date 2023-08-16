9170a5dc4e9565903819b90fbdfc7a71b3d78058
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-08-15 19:34:36 -0700

    updated tasks

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,5 @@
 --this script that lists the number of records with the same score
-SELECT score, COUNT(score) as 'number' FROM second_table GROUP BY score ORDER BY `number` DESC;
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
