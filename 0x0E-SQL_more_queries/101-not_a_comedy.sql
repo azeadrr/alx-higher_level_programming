b5d057e3c58fd0bc6f42f5d43d718938cf015ae6
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-08-16 16:36:00 -0700

    advanced tasks

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,11 @@
+-- this script lists all shows without the genre Comedy in the database hbtn_0d_tvshows
+SELECT tv_shows.title AS 'title'
+FROM tv_shows
+WHERE tv_shows.id NOT IN (
+	    SELECT tv_shows.id
+	    FROM tv_shows INNER JOIN tv_show_genres 
+	    ON tv_show_genres.show_id = tv_shows.id 
+	    INNER JOIN tv_genres
+	    ON tv_genres.id = tv_show_genres.genre_id
+	    WHERE tv_genres.name = 'Comedy')
+ORDER BY tv_shows.title ASC;
