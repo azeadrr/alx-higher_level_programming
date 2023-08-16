4a891a5ad75ce874163a19dfda07b145cf65eb12
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-08-16 16:49:48 -0700

    updated

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- this script lists all privileges of the MySQL users
-SHOW GRANTS FOR user_0d_1 @localhost;
-SHOW GRANTS FOR user_0d_2 @localhost;
+-- this script list all privileges of the MySQL users user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
