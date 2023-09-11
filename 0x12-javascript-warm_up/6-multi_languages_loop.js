8ba9b1bdf96f994b852e3069540731d6f0f00a93
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-11 15:35:53 -0700

    updated

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-    console.log(line);
-  }
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+array.forEach((value, index) => {
+  console.log(`${value}`);});
