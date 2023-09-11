9fa2bb8f9146dfc22bdeedcb8334edd9fe930aac
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-11 16:53:14 -0700

    update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 
-const arg = Math.floor(Number(process.argv[2]));
-if (isNaN(arg)) {
-  console.log('Missing number of occurrences');
-} else {
-  for (let i = 0; i < arg; i++) 
-  {
+const arg = process.argv[2];
+const value = parseInt(arg);
+if (!isNaN(value)) {
+  for (let index = 0; index < Number(value); index++) {
     console.log('C is fun');
   }
-}
+} else { console.log('Missing number of occurrences'); }
