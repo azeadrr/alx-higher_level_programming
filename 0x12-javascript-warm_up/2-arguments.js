281a6335372caade715e8cac76e766180ac91f5a
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-11 12:35:01 -0700

    mandatory tasks

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+
+const numArgs = process.argv.length;
+console.log(numArgs === 2 ? 'No argument' : numArgs === 3 ? 'Argument found' : 'Arguments found');
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+for (const phr of ['C is fun', 'Python is cool', 'JavaScript is amazing']) 
+{
+  console.log(phr);
+}
