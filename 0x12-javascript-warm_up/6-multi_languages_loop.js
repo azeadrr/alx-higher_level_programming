28d8b304178e990b32a54517c2f921ad856344da
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-11 15:28:23 -0700

    updated

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (const letter of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-    console.log(letter);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+    console.log(line);
   }
