38f0b4cb25c41f6e07a106945172888fddfdfb29
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-11 16:49:40 -0700

    update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
 const arg = Math.floor(Number(process.argv[2]));
-if (isNaN(arg)) 
-{
+if (isNaN(arg)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < arg; i++) 
