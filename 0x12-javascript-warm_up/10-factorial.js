2532617a7e8b2a252e9c5f40de4d33151d12e7f1
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-11 17:16:32 -0700

    update

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -5,5 +5,5 @@ const value = parseInt(arg);
 if (!isNaN(value)) { console.log(`${fact(Number(value))}`); } else { console.log(1); }
 
 function fact (num) {
-	  if (num === 0) { return 1; } else { return (num * fact(num - 1)); }
+  if (num === 0) { return 1; } else { return (num * fact(num - 1)); }
 }
