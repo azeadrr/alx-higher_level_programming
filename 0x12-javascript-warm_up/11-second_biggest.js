66e91aa6bee1c1a4cd98519212e165ffd5a5c2d9
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-11 17:23:45 -0700

    update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv.length <= 3) { console.log(0); } else {
+if (process.argv.length <= 3) { console.log('0'); } else {
   const array = [];
   process.argv.flatMap(function (d) {
-    const lr = parseInt(n);
+    const lr = parseInt(d);
     if (!isNaN(lr)) {
       array.push(lr);
       return lr;
