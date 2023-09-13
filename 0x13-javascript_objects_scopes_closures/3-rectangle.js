8060fbe46dd665db5db2a6876ec72c3eb85d1495
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-12 17:28:12 -0700

    mandatory tasks

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,11 +3,18 @@
 class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
-      [this.width, this.height] = [w, h];
+      this.width = w;
+      this.height = h;
     }
   }
 
   print () {
-    for (let index = 0; index < this.height; index++) console.log('X'.repeat(this.width)); }
+    for (let index = 0; index < this.height; index++) {
+      let ph = '';
+      for (let inf = 0; inf < this.width; inf++) {
+        ph += 'X';
+      }
+      console.log(ph);
+    }
+  }
 }
-module.exports = Rectangle;
