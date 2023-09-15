3712d58b0a23a4883c9edb4dc22fe17550b344b6
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-15 15:40:16 -0700

    mandatory tasks

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,7 +2,6 @@
 """
 script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
 """
-
 import MySQLdb
 import sys
 
@@ -18,4 +17,3 @@ if __name__ == "__main__":
         print(row)
     cur.close()
     db.close()
-
