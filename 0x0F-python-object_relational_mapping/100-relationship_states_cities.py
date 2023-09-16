f57832f8e2a7c5bc53e6004884c69f35e96128ab
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-16 15:48:12 -0700

    advanced

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -12,9 +12,10 @@ import sys
 
 if __name__ == '__main__':
     meta = Base.metadata
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True, pool_recycle=False)
+    engine = create_engine('
+            mysql+mysqldb://{}:{}@localhost/{}'
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True, pool_recycle=False)
     meta.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
