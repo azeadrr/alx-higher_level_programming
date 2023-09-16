3f30d31bd9f9c459bb889f767a4c1f4086ad35ab
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-16 16:24:17 -0700

    advanced

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-script that creates the State """
+script that lists all State objects, and corresponding
+City objects, contained in the database hbtn_0e_101_usa
+"""
 import sys
 from relationship_city import Base, City
 from relationship_state import State
@@ -14,9 +16,12 @@ if __name__ == '__main__':
         'mysql+mysqldb://{}:{}@localhost/{}'
         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
         pool_pre_ping=True, pool_recycle=False)
-    meta.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    session.add(City(name="San Francisco", state=State(name="California")))
-    session.commit()
+
+    st_ct = session.query(State).order_by(State.id)
+    for state in st_ct:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
     session.close()
