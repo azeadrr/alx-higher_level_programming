2db7b0e21f7e98e0b5b59d8cecf527bd5cbf5ddd
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-16 15:37:18 -0700

    advanced

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """
-script that creates the State “California” with
-the City “San Francisco” from the database hbtn_0e_100_usa
+this module creates the State “California” with the
+City “San Francisco” from the database.
 """
-
-import sys
-from relationship_state import State
 from relationship_city import Base, City
-from sqlalchemy.orm import sessionmaker
+from relationship_state import State
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
 
 
 if __name__ == '__main__':
     meta = Base.metadata
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                             pool_pre_ping=True, pool_recycle=False)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True, pool_recycle=False)
     meta.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
