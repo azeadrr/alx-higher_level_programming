801b5d54da9c7fd623b1d77353e759a371899fb1
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-16 16:59:10 -0700

    mandatory

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa"""
+"""script that takes in arguments and displays all value"""
 import MySQLdb
 import sys
 
