fb44b9adacbd876f82cc73d8fb953812da536817
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-17 11:58:26 -0700

    mandatory

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,9 +3,9 @@
 from the database hbtn_0e_6_usa"""
 
 import sys
+from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 
 if __name__ == "__main__":
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).order_by(State.id):
-        print(instance.id, instance.name, net=": ")
+    for cities in session.query(State).order_by(State.id):
+        print(cities.id, cities.name, net=": ")
