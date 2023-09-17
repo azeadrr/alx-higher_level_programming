44238ab1eb4c6ad57ed769b3b176215ed0cffb62
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-17 14:04:14 -0700

    mandatory

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa"""
+"""script that lists all State objects that
+contain the letter a from the database hbtn_0e_6_usa"""
 
 import sys
 from sqlalchemy.orm import sessionmaker
@@ -11,6 +12,7 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-    session = sessionmaker(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
     for cities in session.query(State).filter(State.name.like('%a%')):
-        print(cities.id, cities.name, net=": ")
+        print(cities.id, cities.name, sep=": ")
