49d69ec6c072a5ec4f547cee87338ddb3660486a
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-17 14:41:42 -0700

    mandatory

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """
-Python file similar to model_state.py named model_city.py
-that contains the class definition of a City
+Python file similar to model_state.py 
+named model_city.py
+that contains class definition of a City
 """
 
-from model_state import Base
 from sqlalchemy import Column, String, ForeignKey, Integer
 from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
 
 class City(Base):
-    """
-    Class city
-    """
+    """Class city"""
     __tablename__ = 'cities'
     id = Column(Integer, nullable=False, unique=True, primary_key=True)
     name = Column(String(128), nullable=False)
