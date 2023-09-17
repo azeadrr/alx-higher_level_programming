f3e86cf848f1ca0fce547b3595dbeaef1675e0bd
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-17 16:04:33 -0700

    mandatory

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -3,10 +3,10 @@
 from the database hbtn_0e_14_usa"""
 
 import sys
+from model_city import City
+from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-from model_city import City
 
 
 if __name__ == "__main__":
