1b3d28f56f481f9e4d194e50b984400dd46863c1
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-17 14:10:46 -0700

    mandatory

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -11,11 +11,11 @@ from sqlalchemy import (create_engine)
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine) 
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    cities = session.query(State).filter(State.name == (sys.argv[4],))
+    instance = session.query(State).filter(State.name == (sys.argv[4],))
     try:
-        print(cities[0].id)
+        print(instance[0].id)
     except IndexError:
         print("Not found")
