f0cb06a26103b33dfa6de8190c1bcb9609fc1fec
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-17 14:15:08 -0700

    mandatory

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).filter(State.name == (sys.argv[4],))
+    cities = session.query(State).filter(State.name == (sys.argv[4],))
     try:
-        print(instance[0].id)
+        print(cities[0].id)
     except IndexError:
         print("Not found")
