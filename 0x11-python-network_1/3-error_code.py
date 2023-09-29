478e425f4d4005db4535fb14afe7bf42c7692c71
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-29 12:57:51 -0700

    mandatory tasks

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,10 +3,9 @@
 sends request to URL and displays body"""
 
 if __name__ == '__main__':
-from sys import argv
 from urllib.request import urlopen
 from urllib.error import HTTPError
-
+from sys import argv
     try:
         with urlopen(argv[1]) as res:
             print(res.read().decode('utf-8'))
