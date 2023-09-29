b20cb05e7ad4672c4def3ced6e6eed209ebf9b5d
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-28 18:02:37 -0700

    advanced tasks

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# makes a request to 0.0.0.0:5000/catch_me and server responds with a message
-curl -sL -X PUT -H 'Origin: School' 0:5000/catch_me_3
+#Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond
+curl -sL -X PUT -H 'You got me!' 0:5000/catch_me_3
