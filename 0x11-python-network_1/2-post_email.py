bc5570c8a36becc6181e423d3bbea151d4c1686d
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-29 11:48:26 -0700

    mandatory tasks

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """Python script that takes in URL and email,
 sends a POST request to passed URL with email"""
+
 if __name__ == "__main__":
-    from sys import argv
     from urllib.request import urlopen, Request
     from urllib.parse import urlencode
+    from sys import argv
 
     url = argv[1]
-    requestrl = Request(url, data=data)
     email = {'email': argv[2]}
     data = urlencode(email).encode('utf-8')
+    requestrl = Request(url, data=data)
     with urlopen(requestrl) as res:
         nes = res.read().decode('utf-8')
         print(nes)
