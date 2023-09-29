ca3334f64f3c46434f7733f7f7525568edb25e2d
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-29 12:52:42 -0700

    mandatory tasks

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,12 +2,12 @@
 """Python script that takes in URL
 sends request to URL and displays body"""
 
-from sys import argv
+if __name__ == '__main__':
 from urllib.request import urlopen
 from urllib.error import HTTPError
+from sys import argv
 
 
-if __name__ == '__main__':
     try:
         with urlopen(argv[1]) as res:
             print(res.read().decode('utf-8'))
