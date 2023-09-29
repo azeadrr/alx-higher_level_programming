63a55e23977b36135d599c549708b3e8296f5382
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-29 11:22:06 -0700

    mandatory tasks

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id"""
+"""Python script that takes in a URL, sends
+request to URL and displays value of X-Request-Id"""
 
 if __name__ == "__main__":
     from urllib.request import urlopen
