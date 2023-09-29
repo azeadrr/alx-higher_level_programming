2a534922b6f432db1db2717dfd210e0c66a362ae
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-09-29 12:50:03 -0700

    mandatory tasks

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,15 +2,14 @@
 """Python script that takes in URL
 sends request to URL and displays body"""
 
+from sys import argv
+from urllib.request import urlopen
+from urllib.error import HTTPError
 
-if __name__ == "__main__":
-    from urllib.request import urlopen
-    from urllib.error import HTTPError
-    from sys import argv
 
+if __name__ == '__main__':
     try:
         with urlopen(argv[1]) as res:
-             nes = res.read().decode('utf-8')
-             print(nes)
+            print(res.read().decode('utf-8'))
     except HTTPError as er:
         print('Error code: {}'.format(er.code))
