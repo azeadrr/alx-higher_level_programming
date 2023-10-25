549273cc3d289ad3fbd5c9f3df6e61d698ae6873
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-10-24 18:40:06 -0700

    advanced tasks

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,17 +2,17 @@
 
 const reqst = require('request');
 const apiStarWar = 'https://swapi-api.alx-tools.com/api/films/'.concat(process.argv[2]);
-const retrive = (array, idx) => {
+const retv = (array, idx) => {
   if (idx === array.length) return;
   reqst(array[idx], (err, resp, body) => {
     if (err) console.log(err);
     console.log(JSON.parse(body).name);
-    retrive(array, idx + 1);
+    retv(array, idx + 1);
   });
 };
 
 reqst(apiStarWar, (err, _response, body) => {
   if (err) console.log(err);
   const res = JSON.parse(body).characters;
-  retrive(res, 0);
+  retv(res, 0);
 });
