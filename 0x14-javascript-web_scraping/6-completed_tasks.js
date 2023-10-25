01dd135bbb254d3a11c04433e407bcc9c36ba450
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-10-24 18:28:05 -0700

    mandatory tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,24 +1,23 @@
 #!/usr/bin/node
-
 const reqst = require('request');
 reqst(process.argv[2], (err, _resp, body) => {
   if (err) {
     console.log(err);
   } else {
-    const tskDone = {};
+    const tasksDone = {};
     const res = JSON.parse(body);
     let idx = 0;
     while (idx < res.length) {
       const userId = res[idx].userId;
-      const cmplt = res[idx].cmplt;
-      if (cmplt && !tskDone[userId]) {
-        tskDone[userId] = 0;
+      const completed = res[idx].completed;
+      if (completed && !tasksDone[userId]) {
+        tasksDone[userId] = 0;
       }
-      if (cmplt) {
-        ++tskDone[userId];
+      if (completed) {
+        ++tasksDone[userId];
       }
-      idx += 1;
+      idx = idx + 1;
     }
-    console.log(tskDone);
+    console.log(tasksDone);
   }
 });
