eef6f7ce67fd4e58ea2f85d528ec4022a0472e91
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-10-24 17:40:32 -0700

    mandatory tasks

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', (error, result) {
+fs.readFile(process.argv[2], 'utf8', (error, result) => {
 	if (error) {
     console.log(error);
   } else {
